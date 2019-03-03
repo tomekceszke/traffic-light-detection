37d504b3af552775dbb105682dd3261f5df5fe9f
tomekceszke <tomek@ceszke.com>
2019-03-03 22:17:56 +0100

    fixed traffic light coordinates

--- bin/startStream.bat
+++ bin/startStream.bat
@@ -3,4 +3,4 @@
 
 :: Author: Tomasz Ceszke 2017
 
-"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe" http://ls.tkchopin.pl:1935/live/orzeszkowa.stream/playlist.m3u8 --network-caching=2000 --no-audio --video-filter=croppadd:scene --croppadd-croptop=136 --croppadd-cropbottom=830 --croppadd-cropleft=1550 --croppadd-cropright=286 --scene-format=png --scene-path="..\datasource\live" --scene-prefix=scene --scene-ratio=25 --scene-replace 
+"C:\Program Files\VideoLAN\VLC\vlc.exe" http://ls.tkchopin.pl:1935/live/orzeszkowa.stream/playlist.m3u8 --network-caching=2000 --no-audio --video-filter=croppadd:scene --croppadd-croptop=90 --croppadd-cropbottom=860 --croppadd-cropleft=1050 --croppadd-cropright=770 --scene-format=png --scene-path="..\datasource\live" --scene-prefix=scene --scene-ratio=25 --scene-replace 
